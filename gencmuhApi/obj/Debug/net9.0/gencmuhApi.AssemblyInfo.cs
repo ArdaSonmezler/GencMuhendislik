8e48c694f21a4537490bcf278033546d0b82f382
Tulin Taspinar <tulinttaspinar@gmail.com>
2025-02-01 21:11:55 +0300

    Blog,Career,Image,ImageCategory,Slider and Youtube classes has been created

--- gencmuhApi/obj/Debug/net9.0/gencmuhApi.AssemblyInfo.cs
+++ gencmuhApi/obj/Debug/net9.0/gencmuhApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gencmuhApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36d1e86fe759cf6429fec08933489b3de75d7a06")]
 [assembly: System.Reflection.AssemblyProductAttribute("gencmuhApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gencmuhApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
